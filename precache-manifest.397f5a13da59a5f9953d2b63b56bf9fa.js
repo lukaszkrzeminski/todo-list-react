41f4838f8f246e72a15da08716be88ae50015384
Lukasz Krzeminski <krzeminski.lukasz@interia.pl>
2020-07-18 15:46:55 +0200

    Updates

---
+++ precache-manifest.397f5a13da59a5f9953d2b63b56bf9fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400a44cbeb28b19a1cbe4505fd75d8b4",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "1a82d3e6cd439c5c1883",
+    "url": "/todo-list-react/static/css/main.33809279.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a82d3e6cd439c5c1883",
+    "url": "/todo-list-react/static/js/main.19ebd3c6.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
